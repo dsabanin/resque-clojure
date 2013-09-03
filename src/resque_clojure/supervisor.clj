5cb0714bdce5b37d801d667d60eeb6b13623da4b
Dima Sabanin <sdmitry@gmail.com>
2013-09-03 13:55:21 -0400

    Configurable error handler for dispatching thread errors. This will allow you to specify what to do when exception kills the dispatching thread and effectively all processing of new jobs.

--- src/resque_clojure/supervisor.clj
+++ src/resque_clojure/supervisor.clj
@@ -1,17 +1,19 @@
 (ns resque-clojure.supervisor
   (:require [resque-clojure.worker :as worker]
-            [resque-clojure.resque :as resque]))
+            [resque-clojure.resque :as resque]
+            [resque-clojure.util :as util]))
 
 (def run-loop? (ref true))
 (def working-agents (ref #{}))
 (def idle-agents (ref #{}))
 (def watched-queues (atom []))
+(def dispatching-thread (atom nil))
 
 (def config (atom {:max-shutdown-wait (* 10 1000) ;; milliseconds
                    :poll-interval (* 5 1000)
                    :max-workers 1}))
 
-(declare release-worker reserve-worker make-agent listen-to listen-loop dispatch-jobs)
+(declare release-worker reserve-worker make-agent listen-to listen-loop dispatch-jobs stop-dispatching-thread! create-dispatching-thread!)
 
 (defn configure [c]
   (swap! config merge c))
@@ -20,16 +22,35 @@
   "stops polling queues. waits for all workers to complete current job"
   (dosync (ref-set run-loop? false))
   (apply await-for (:max-shutdown-wait @config) @working-agents)
-  (resque/unregister @watched-queues))
+  (resque/unregister @watched-queues)
+  (stop-dispatching-thread!))
 
-(defn start [queues]
-  "start listening for jobs on queues (vector)."
-  (dotimes [n (:max-workers @config)] (make-agent))
-  (listen-to queues)
-  (dosync (ref-set run-loop? true))
-  (.start (Thread. listen-loop (str "Listening to " (prn-str queues))))
-  (.addShutdownHook (Runtime/getRuntime)
-                    (Thread. stop)))
+(defn default-dispatching-error-handler [e]
+  (util/print-exception e)
+  (throw e))
+
+(defn start
+  ([queues]
+     (start queues {:listener-error-handler default-dispatching-error-handler}))
+  ([queues {:keys [listener-error-handler]}]
+     "start listening for jobs on queues (vector)."
+     (dotimes [n (:max-workers @config)] (make-agent))
+     (listen-to queues)
+     (dosync (ref-set run-loop? true))
+     (create-dispatching-thread! queues listener-error-handler)
+     (.addShutdownHook (Runtime/getRuntime)
+                       (Thread. stop))))
+
+(defn create-dispatching-thread! [queues listener-error-handler]
+  (let [thread (Thread. (build-listen-loop listener-error-handler)
+                        (str "Dispatching from " (prn-str queues)))]
+    (reset! dispatching-thread thread)
+    (.start thread)))
+
+(defn stop-dispatching-thread! []
+  (when @dispatching-thread
+    (.interrupt @dispatching-thread)
+    (reset! dispatching-thread nil)))
 
 (defn worker-complete [key ref old-state new-state]
   (release-worker ref)
@@ -44,10 +65,13 @@
         (send-off worker-agent worker/work-on msg)
         (release-worker worker-agent)))))
 
-(defn listen-loop []
-  (if @run-loop?
-    (do
-      (dispatch-jobs)
+(defn build-listen-loop [error-handler]
+  (fn []
+    (when @run-loop?
+      (try
+        (dispatch-jobs)
+        (catch Exception e
+          (error-handler e)))
       (Thread/sleep (:poll-interval @config))
       (recur))))
 
--- src/resque_clojure/util.clj
+++ src/resque_clojure/util.clj
@@ -6,3 +6,11 @@
 (defn filter-map [map keys]
   "return a new map containing only the key/value pairs that match the keys"
   (into {} (filter (fn [[k v]] (includes? keys k)) map)))
+
+(defn print-exception [e]
+  (println "Exception:" e
+           "\nTrace:\n"
+           (->> (.getStackTrace e)
+                (map #(str "\t" %))
+                (clojure.string/join "\n"))
+           "\n"))
