c088cc111b50af5e824ecad22c6168490954371d
Dima Sabanin <sdmitry@gmail.com>
2013-09-08 14:35:31 -0400

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dsabanin/resque-clojure "0.3.2"
+(defproject dsabanin/resque-clojure "0.3.4"
   :description "Redis based library for asynchronous processing"
   :url "https://github.com/jxa/resque-clojure"
   :license {:name "The MIT License"
